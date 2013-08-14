d72df38413591316397ae22696ac09b964a53584
Luiz Cezer <lccezinha@gmail.com>
2013-08-13 22:10:56 -0300

    recipes pt 2

--- ebook_howto/dsl/recipe.rb
+++ ebook_howto/dsl/recipe.rb
@@ -1,12 +1,23 @@
+#coding : utf-8
 class Recipe
   attr_accessor :name
   attr_accessor :description
   attr_accessor :ingredients
   attr_accessor :instructions
   
-  def initialize
+  def initialize(&block)
     @ingredients = []
     @instructions = []
-  end
 
+    instance_eval &block if block_given?
+    self
+  end
+end
+
+recipe = Recipe.new do 
+  self.name = 'Coca'
+  self.ingredients << 'Açucar'
+  self.ingredients << 'Sal'
+  self.ingredients << 'Muito corante'
+  self.ingredients << 'Coisas misteriosas'
 end
