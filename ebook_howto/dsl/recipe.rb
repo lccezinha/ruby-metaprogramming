f909a57faf7577dafd010efcc75df83fb574c79b
Luiz Cezer <lccezinha@gmail.com>
2013-08-13 22:50:25 -0300

    interface fluente. SQL pt 1

--- ebook_howto/dsl/recipe.rb
+++ ebook_howto/dsl/recipe.rb
@@ -1,9 +1,20 @@
 #coding : utf-8
+class Object
+  def self.dsl_attr(name)
+    class_eval <<-RUBY, __FILE__, __LINE__
+    def #{name}(*args)
+      @#{name} = args.first unless args.empty?
+      @#{name}
+    end
+    RUBY
+  end
+end
+
 class Recipe
-  attr_accessor :name
-  attr_accessor :description
-  attr_accessor :ingredients
-  attr_accessor :instructions
+  dsl_attr :name
+  dsl_attr :description
+  dsl_attr :ingredients
+  dsl_attr :instructions
   
   def initialize(&block)
     @ingredients = []
@@ -12,12 +23,42 @@ class Recipe
     instance_eval &block if block_given?
     self
   end
+
+  def to_text
+    "".tap do |text|
+      text << name
+      text << "\n" << ("=" * name.size) << "\n"
+
+      text << "\n" << description if description
+      text << "\n"
+
+      text << "\nIngredientes" << "\n-----------\n"
+      text << ingredients.map { |ing| "- #{ing}\n" }.join("")
+
+      text << "\nComo preparar" << "\n-----------\n"
+      text << instructions.each_with_index.map { |item, index| "#{index + 1}. #{item}\n" }.join("")
+    end
+  end
 end
 
+# recipe = Recipe.new do 
+#   self.name = 'Coca'
+#   self.ingredients << 'Açucar'
+#   self.ingredients << 'Sal'
+#   self.ingredients << 'Muito corante'
+#   self.ingredients << 'Coisas misteriosas'
+# end
+
 recipe = Recipe.new do 
-  self.name = 'Coca'
-  self.ingredients << 'Açucar'
-  self.ingredients << 'Sal'
-  self.ingredients << 'Muito corante'
-  self.ingredients << 'Coisas misteriosas'
+  name 'LOL'
+  description 'UMA BAGAÇA AE'
+
+  ingredients << 'Uma meia'
+  ingredients << '3 Colher'
+  ingredients << '4 Pombas'
+
+  instructions << 'Mexe tudo ve bolinha'
+  instructions << 'Serve a gosto'
 end
+
+puts recipe.to_text
---
+++ ebook_howto/dsl/sql.rb
@@ -0,0 +1,36 @@
+# para criar interfaces fluentes, como por exemplo o AREL que permiter Class.select(...).where(...).order('...')
+# é preciso sempre retornar o receiver após definir o attr.
+
+class SQL < BasicObject
+  def select(fields)
+    @select = fields
+    self
+  end
+
+  def from(tables)
+    @tables = tables
+    self
+  end
+
+  def where(conditions)
+    @where = conditions
+    self
+  end
+
+  def to_sql
+    "".tap do |query|
+      query << "SELECT #{@select} FROM #{@tables} "
+      query << "WHERE #{build_conditions}" if @where
+    end
+  end
+
+  private
+
+  def build_conditions
+    @where.collect { |name, value| %[#{name} = "#{value}"]}.join(" AND ")
+  end
+end
+
+sql = SQL.new
+sql.select("*").from("users").where(:name => 'Cezinha')
+puts sql.to_sql
